3924b20ca493f43d2b16e0b21735f69837841832
Dreamer <33318520+hhrum@users.noreply.github.com>
2021-04-11 16:48:50 +0500

    кринж

--- src/controllers/Vacancy.js
+++ src/controllers/Vacancy.js
@@ -1,11 +1,32 @@
 // Модели пока что нет
 const Vacancy = require("../models/Vacancy");
+const Connect = require("../models/VacancyResumeConnect");
 
 class VacancyControler {
   async index(req, res) {
     const resume = req.user.resume;
 
-    res.send(await Vacancy.find().populate("company").populate("profession"));
+    let vacancies = await Vacancy.find({ profession: resume.profession })
+      .populate("company")
+      .populate("profession");
+
+    let exVacs = await Connect.find({
+      resume: resume._id,
+      resumeState: { $ne: null }
+    });
+
+    vacancies = vacancies.filter((vac) => {
+      const test = !!exVacs.find((el) => {
+        console.log(el.vacancy, vac._id, el.vacancy === vac._id);
+
+        return el.vacancy === vac._id;
+      });
+      console.log(test);
+
+      return !!test;
+    });
+
+    res.send(vacancies);
   }
 
   show(req, res) {}
